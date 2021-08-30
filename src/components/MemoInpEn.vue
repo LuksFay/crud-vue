8d371d0e1f115afbd47f2669ba0392c3fef5ce56
LuksFay <lukspicco@hotmail.com>
2021-08-30 17:30:08 -0300

    borra y edita, hay que hacer un modal

--- src/components/MemoInpEn.vue
+++ src/components/MemoInpEn.vue
@@ -1,13 +1,12 @@
 <template>
-    <div>
+    <div class="container">
         <div>
             <h1>Leave a MEMO</h1>
-            <div>     
-                    <input type="text" name="Memo title"  placeholder="Write here your MEMO" maxlength="27" v-model="writeMemo">
-                    <textarea name="" id="" cols="25" rows="5" maxlength="280" v-model="writeMemoBody"></textarea>  
-                         
+            <div class="container memo-inputs">     
+                    <input type="text" name="Memo title"  placeholder="Title" maxlength="27" v-model="writeMemo">
+                    <textarea cols="25" rows="5" maxlength="280" v-model="writeMemoBody" placeholder="Body"></textarea>  
+                    <button @click="createMemit">MEM-it</button>     
             </div>
-            <button @click="createMemit">MEM-it</button>
             <MemoResEn :previewMemo="writeMemo"/>
             <MemoResEn :previewMemo="writeMemoBody"/>
         </div>
@@ -16,12 +15,14 @@
             <router-link to ="/memo-pt">Português</router-link>
         </div>
         <div class="memits-container container">
-            <div v-for="(memit,index) in memits" :key='index' class="mem-it">
+            <div v-for="(memit) in memits" :key='memit.id' class="mem-it">
                 <div>
                <h3>{{ memit.title }}</h3> 
                 <p>{{ memit.body }}</p>
-                <button @click="editMemit">Edit</button>
-                <button @click="deleteMemit">X</button>
+                <div class="button-position">
+                    <button @click="editMemit(memit.id, memit.title, memit.body)">Edit</button>
+                    <button @click.capture="deleteMemit(memit.id, memit.title)">X</button>
+                </div>
                 </div>
             </div>
         </div>
@@ -40,39 +41,59 @@ export default {
             writeMemo: "",
             writeMemoBody: "",
             memits: [],
+             
         }
     },
     methods: {
 
         createMemit() {
             this.memits.push({
+                id: Math.random() + Math.random(),
                 title:this.writeMemo,
                 body:this.writeMemoBody
             });
             this.writeMemo='';
             this.writeMemoBody='';
-            localStorage.setItem('old-memits',JSON.stringify(this.memits));              
+            localStorage.setItem('all-memits',JSON.stringify(this.memits));              
         },
-        editMemit(){
-            console.log("This edit the memit")
-            console.log(this.memits)
-            localStorage.setItem('old-memits',JSON.stringify(this.memits));
-        },
-        deleteMemit(index){
-            console.log("this delete the memit");
-            let areUSure = prompt("are u sure of deleting this memit? D: \nIf the answer is 'yes': \nwrite 'yes' bellow (whitout the ' ' ) ");
-            if(areUSure === 'yes'){
-                this.memits.splice(index,1)   
-            }else{
-                console.log("the memit survived to the assassination attempt");
-                return
-            }
-            localStorage.setItem('old-memits',JSON.stringify(this.memits));
-        }
+        deleteMemit(id){
+           let nodeletes = this.memits.filter(memit => memit.id != id);
+            localStorage.setItem('all-memits',JSON.stringify(nodeletes))
+            let saveMemit = JSON.parse(localStorage.getItem('all-memits'));
+                if(saveMemit === null){
+                    this.memits = [];
+                }else{
+                    this.memits = saveMemit;
+                }
+            },
+        editMemit(id, title, body){
+            console.log("This edit the memit");
+            console.log('original memit: ', id , title, body);
+            let newarray = this.memits.map(memit => {
+                if(memit.id === id){
+                   return {
+                       
+                       id: memit.id,
+                       title: prompt('Nuevo Titulo para el memit', title),
+                       body: prompt('Nuevo cuerpo para el memit', body)   
+                   }
+                }else{
+                    return memit
+                }
+            })
+            console.log(newarray);
+            localStorage.setItem('all-memits',JSON.stringify(newarray))
+            let saveMemit = JSON.parse(localStorage.getItem('all-memits'));
+                if(saveMemit === null){
+                    this.memits = [];
+                }else{
+                    this.memits = saveMemit;
+                }
+        }   
 
     },
     created(){
-                let saveMemit = JSON.parse(localStorage.getItem('old-memits'));
+                let saveMemit = JSON.parse(localStorage.getItem('all-memits'));
                 if(saveMemit === null){
                     this.memits = [];
                 }else{
@@ -93,12 +114,25 @@ export default {
     .memits-container{
         display: flex;
         gap: 20px;
+        flex-wrap: wrap;
     }
     .mem-it{
         background: #74ff34;
         min-width: 10px;
+        max-width: 300px;
+        word-break: break-word;
+        padding: 20px;
     }
     textarea {
         resize: none;
     }
+    .memo-inputs{
+        display: flex;
+        flex-direction: column;
+        max-width: 30%;
+        gap: 10px;
+    }
+    
+    
+    
 </style>
